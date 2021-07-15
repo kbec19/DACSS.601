140cb2207d2b09230581defbe4080784396afcb4
Kristina <kbecvar@umass.edu>
2021-07-14 19:22:25 -0500

    Cleanup

---
+++ homework_submission.R
@@ -0,0 +1,20 @@
+library(blogbuilder)
+library(distill)
+iamstudent()
+
+# pull the repository from git
+create.post("New Post", draft = TRUE)
+
+# do the homework
+# knit the homework
+
+# pull the repository again before submitting
+# commit the changes
+
+# if there are docs in the commit list, highlight and revert
+# push to the repository
+
+
+
+
+
