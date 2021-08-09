ef6f2d24c2328bd201f527a2a265ad59e108c091
Kristina <kbecvar@umass.edu>
2021-08-08 22:09:32 -0500

    Wrapping up HW5

---
+++ posts/Corrplot.R
@@ -0,0 +1,3 @@
+## **Different ways of visualizing variable correlations**
+
+I also found another way to look at the Pearson correlation coefficient of each relationship in my data set. The package corrplot gives some great tools to not only calculate, but visualize those correlations between all of the variables in the data set.
